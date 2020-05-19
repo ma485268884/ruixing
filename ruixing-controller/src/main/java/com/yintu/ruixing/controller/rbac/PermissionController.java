d2036e12ad7b0207372864150065eb9af73bfa32
global_mwy <global_1107201045@qq.com>
2020-05-19 17:50:44 +0800

    Edit too much file

---
+++ ruixing-controller/src/main/java/com/yintu/ruixing/controller/rbac/PermissionController.java
@@ -0,0 +1,51 @@
+package com.yintu.ruixing.controller.rbac;
+
+import com.yintu.ruixing.common.util.ResponseDataUtil;
+import com.yintu.ruixing.entity.rbac.PermissionEntity;
+import com.yintu.ruixing.entity.rbac.RoleEntity;
+import com.yintu.ruixing.service.rbac.PermissionService;
+import com.yintu.ruixing.service.rbac.RoleService;
+import com.yintu.ruixing.service.rbac.impl.PermissionServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * @author:mlf
+ * @date:2020/5/19 17:40
+ */
+@RestController
+public class PermissionController {
+    @Autowired
+    private PermissionService permissionService;
+
+
+    @PostMapping("/permissions")
+    public Map<String, Object> add(PermissionEntity permissionEntity) {
+        Assert.notNull(permissionEntity.getParentId(), "父级id不能为空");
+        Assert.notNull(permissionEntity.getUrl(), "URL不能为空");
+        Assert.notNull(permissionEntity.getMethod(), "请求方法不能为空");
+        permissionService.add(permissionEntity);
+        return ResponseDataUtil.ok("添加权限成功");
+    }
+
+    @DeleteMapping("/permissions/{id}")
+    public Map<String, Object> remove(@PathVariable Long id) {
+        permissionService.remove(id);
+        return ResponseDataUtil.ok("删除权限成功");
+    }
+
+    @PutMapping("/permissions/{id}")
+    public Map<String, Object> edit(@PathVariable Long id, PermissionEntity permissionEntity) {
+        permissionService.edit(permissionEntity);
+        return ResponseDataUtil.ok("修改权限成功");
+    }
+
+    @GetMapping("/permissions/{id}")
+    public Map<String, Object> find(@PathVariable Long id) {
+        PermissionEntity permissionEntity = permissionService.findById(id);
+        return ResponseDataUtil.ok("查询权限成功", permissionEntity);
+    }
+}
--- ruixing-controller/src/main/java/com/yintu/ruixing/controller/rbac/UserController.java
+++ ruixing-controller/src/main/java/com/yintu/ruixing/controller/rbac/UserController.java
@@ -1,14 +1,45 @@
 package com.yintu.ruixing.controller.rbac;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yintu.ruixing.common.util.ResponseDataUtil;
+import com.yintu.ruixing.dao.rbac.UserDao;
+import com.yintu.ruixing.entity.rbac.UserEntity;
+import com.yintu.ruixing.service.rbac.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    public String test() {
-        return "HelloWorld";
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    public Map<String, Object> add(UserEntity userEntity) {
+        Assert.notNull(userEntity.getUsername(), "用户名不能为空");
+        Assert.notNull(userEntity.getPassword(), "密码不能为空");
+        userService.add(userEntity);
+        return ResponseDataUtil.ok("添加用户成功");
     }
+
+    @DeleteMapping("/users/{id}")
+    public Map<String, Object> remove(@PathVariable Long id) {
+        userService.remove(id);
+        return ResponseDataUtil.ok("删除用户成功");
+    }
+
+    @PutMapping("/users/{id}")
+    public Map<String, Object> edit(@PathVariable Long id, UserEntity userEntity) {
+        userService.edit(userEntity);
+        return ResponseDataUtil.ok("修改用户成功");
+    }
+
+    @GetMapping("/users/{id}")
+    public Map<String, Object> find(@PathVariable Long id) {
+        UserEntity userEntity = userService.findById(id);
+        return ResponseDataUtil.ok("查询用户成功", userEntity);
+    }
+
 }
