6318ebb0c36ed66f18b332509f307bf5c87643f8
global_mwy <global_1107201045@qq.com>
2020-05-20 22:31:56 +0800

    Edit SpringSecurityConfiguration.java

---
+++ ruixing-controller/src/main/java/com/yintu/ruixing/component/CustomWebMvcConfigurer.java
@@ -0,0 +1,18 @@
+package com.yintu.ruixing.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class CustomWebMvcConfigurer extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3600);
+    }
+}
