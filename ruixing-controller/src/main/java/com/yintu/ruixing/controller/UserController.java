c4e6b2dc398fa24251a09c5a3e4e4440b9015304
global_mwy <global_1107201045@qq.com>
2020-05-21 18:16:01 +0800

    Add RoleServiceImpl.java file

--- ruixing-controller/src/main/java/com/yintu/ruixing/controller/UserController.java
+++ ruixing-controller/src/main/java/com/yintu/ruixing/controller/UserController.java
@@ -1,24 +1,38 @@
 package com.yintu.ruixing.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.yintu.ruixing.common.util.ResponseDataUtil;
 import com.yintu.ruixing.entity.UserEntity;
+import com.yintu.ruixing.service.PermissionService;
 import com.yintu.ruixing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * @author:mlf
+ * @date:2020/5/19 17:20
+ */
 @RestController
-public class UserController {
+public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private PermissionService permissionService;
 
     @PostMapping("/users")
     public Map<String, Object> add(UserEntity userEntity) {
         Assert.notNull(userEntity.getUsername(), "用户名不能为空");
         Assert.notNull(userEntity.getPassword(), "密码不能为空");
+        Assert.notNull(userEntity.getEnableds(), "状态不能为空");
         userService.add(userEntity);
         return ResponseDataUtil.ok("添加用户成功");
     }
@@ -36,9 +50,16 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public Map<String, Object> find(@PathVariable Long id) {
+    public Map<String, Object> findById(@PathVariable Long id) {
         UserEntity userEntity = userService.findById(id);
         return ResponseDataUtil.ok("查询用户成功", userEntity);
     }
-    
+
+    @GetMapping("/users")
+    public Map<String, Object> findAll(@RequestParam("page_number") Integer pageNumber, @RequestParam("page_size") Integer pageSize, @RequestParam(value = "search_text", required = false) String username) {
+        PageInfo<JSONObject> pageInfo = userService.findAllAndUrlByUserIdAndUrl(pageNumber, pageSize, username, this.getLoginUserId(), "/users");
+        return ResponseDataUtil.ok("查询用户列表成功", pageInfo);
+    }
+
+
 }
--- ruixing-controller/src/main/java/com/yintu/ruixing/exception/GlobalExceptionHandler.java
+++ ruixing-controller/src/main/java/com/yintu/ruixing/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.yintu.ruixing.exception;
 
+import com.yintu.ruixing.common.exception.BaseRuntimeException;
 import com.yintu.ruixing.common.util.ResponseDataUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,10 +21,24 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(SQLException.class)
     public Map<String, Object> sqlException(SQLException e) {
-        logger.error(e.getMessage());
         if (e instanceof SQLIntegrityConstraintViolationException) {
             return ResponseDataUtil.error("该数据有关联数据，操作失败");
         }
+        logger.error(e.getMessage());
         return ResponseDataUtil.error("数据库异常，操作失败");
     }
+
+    @ExceptionHandler(BaseRuntimeException.class)
+    public Map<String, Object> baseRuntimeException(BaseRuntimeException e) {
+        logger.error(e.getMessage());
+        return ResponseDataUtil.error(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Map<String, Object> illegalArgumentException(IllegalArgumentException e) {
+        logger.error(e.getMessage());
+        return ResponseDataUtil.error(e.getMessage());
+    }
+
+
 }
