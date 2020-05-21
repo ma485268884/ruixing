d35669a4300acce9d633fddaa96420a9a2c873dd
111 <111>
2020-05-21 11:18:59 +0800

    数据统计

---
+++ ruixing-service/src/main/java/com/yintu/ruixing/service/impl/DataSatsServiceImpl.java
@@ -0,0 +1,11 @@
+package com.yintu.ruixing.service.impl;
+
+import com.yintu.ruixing.service.DataSatsService;
+
+/**
+ * @author:lcy
+ * @date:2020-05-21 11
+ * 数据统计
+ */
+public class DataSatsServiceImpl implements DataSatsService {
+}
