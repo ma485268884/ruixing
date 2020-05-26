bbd89ab36d17aced7fd3a616115c215a51b47f29
global_qyf <global_18574155220@163.com>
2020-05-26 10:51:52 +0800

    ...

---
+++ ruixing-service/src/main/java/com/yintu/ruixing/service/CheZhanService.java
@@ -0,0 +1,31 @@
+package com.yintu.ruixing.service;
+
+import com.yintu.ruixing.entity.CheZhanEntity;
+/**
+ *
+ * @author Qiao
+ * @date 2020/5/25 17:03
+ * @return
+ */
+public interface CheZhanService {
+    /**
+     * @param cheZhanEntity
+     * @author Qiao
+     * @date 2020/5/25 17:04
+     * @return 添加车站信息
+     */
+    void add(CheZhanEntity cheZhanEntity);
+    /**
+     * @author Qiao
+     * @date 2020/5/25 17:04
+     * @return 根据id修改车站信息
+     */
+    void update(Long id);
+
+    /**
+     * @author Qiao
+     * @date 2020/5/25 17:05
+     * @return 根据id查询车站信息
+     */
+    CheZhanEntity findByCheZhanId(Long id);
+}
