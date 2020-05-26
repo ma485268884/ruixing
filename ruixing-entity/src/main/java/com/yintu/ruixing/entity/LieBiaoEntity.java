907fc9c1f3f81e767b60b6c13197e80a3ac49bfc
global_qyf <global_18574155220@163.com>
2020-05-26 16:34:13 +0800

    列表展示

---
+++ ruixing-entity/src/main/java/com/yintu/ruixing/entity/LieBiaoEntity.java
@@ -0,0 +1,24 @@
+package com.yintu.ruixing.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @description:
+ * @author: Qiao
+ * @time: 2020/5/22 15:51
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LieBiaoEntity{
+    private String tlj_name;
+    private String dwd_name;
+    private String xd_name;
+    private String cz_name;
+}
