158f769e3c2823ca92f9ee6db06d4b919b5b87f6
global_mwy <global_1107201045@qq.com>
2020-06-16 15:25:07 +0800

    Edit too much file

--- ruixing-service/src/main/java/com/yintu/ruixing/service/impl/MenXianServiceImpl.java
+++ ruixing-service/src/main/java/com/yintu/ruixing/service/impl/MenXianServiceImpl.java
@@ -1,6 +1,5 @@
 package com.yintu.ruixing.service.impl;
 
-import com.yintu.ruixing.common.exception.BaseRuntimeException;
 import com.yintu.ruixing.dao.MenXianDao;
 import com.yintu.ruixing.entity.MenXianEntity;
 import com.yintu.ruixing.entity.MenXianPropertyEntity;
@@ -33,157 +32,160 @@ public class MenXianServiceImpl implements MenXianService {
     @Override
     public void add(MenXianEntity menXianEntity) {
         Integer quDuanId = menXianEntity.getQuduanId();
-        QuDuanInfoEntity quDuanInfoEntity = quDuanInfoService.findById(quDuanId);
-        if (quDuanInfoEntity != null) {
-            Integer propertyId = menXianEntity.getPropertyId();
-            MenXianPropertyEntity menXianPropertyEntity = menXianPropertyService.findById(propertyId);
-            if (menXianPropertyEntity != null) {
-                MenXianEntity menXianEntity1 = this.findByQuDuanIdAndPropertyId(quDuanId, propertyId);
-                if (menXianEntity1 == null) {
-                    String name = menXianPropertyEntity.getName();
-                    Integer measuredValue;
-                    String measuredValueUnit;
-                    switch (name) {
-                        case "功出电压V":
-                            measuredValue = quDuanInfoEntity.getVOutZhu();
-                            measuredValueUnit = "V";
-                            break;
-                        case "功出电流mA":
-                            measuredValue = quDuanInfoEntity.getMaOutZhu();
-                            measuredValueUnit = "mA";
-                            break;
-                        case "上边频Hz":
-                            measuredValue = quDuanInfoEntity.getHzUpZhu();
-                            measuredValueUnit = "Hz";
-                            break;
-                        case "下边频Hz":
-                            measuredValue = quDuanInfoEntity.getHzDownZhu();
-                            measuredValueUnit = "Hz";
-                            break;
-                        case "发送低频Hz":
-                            measuredValue = quDuanInfoEntity.getHzLowZhu();
-                            measuredValueUnit = "Hz";
-                            break;
-                        case "送端电缆侧电压V":
-                            measuredValue = quDuanInfoEntity.getVSongduanCable();
-                            measuredValueUnit = "V";
-                            break;
-                        case "送端电缆侧电流mA":
-                            measuredValue = quDuanInfoEntity.getMaSongduanCable();
-                            measuredValueUnit = "mA";
-                            break;
-                        case "受端电缆侧主电压V":
-                            measuredValue = quDuanInfoEntity.getVShouduanCableHost();
-                            measuredValueUnit = "V";
-                            break;
-                        case "受端电缆侧调电压V":
-                            measuredValue = quDuanInfoEntity.getVShouduanCableSpare();
-                            measuredValueUnit = "V";
-                            break;
-                        case "轨入电压V":
-                            measuredValue = quDuanInfoEntity.getVInAll();
-                            measuredValueUnit = "V";
-                            break;
-                        case "主接入电压mV":
-                            measuredValue = quDuanInfoEntity.getMvInZhu();
-                            measuredValueUnit = "mV";
-                            break;
+        List<QuDuanInfoEntity> quDuanInfoEntities = quDuanInfoService.findQidAndTime(quDuanId, null);
+        if (quDuanInfoEntities.size() > 0) {
+            QuDuanInfoEntity quDuanInfoEntity = quDuanInfoEntities.get(0);
+            if (quDuanInfoEntity != null) {
+                Integer propertyId = menXianEntity.getPropertyId();
+                MenXianPropertyEntity menXianPropertyEntity = menXianPropertyService.findById(propertyId);
+                if (menXianPropertyEntity != null) {
+                    MenXianEntity menXianEntity1 = this.findByQuDuanIdAndPropertyId(quDuanId, propertyId);
+                    if (menXianEntity1 == null) {
+                        String name = menXianPropertyEntity.getName();
+                        Integer measuredValue;
+                        String measuredValueUnit;
+                        switch (name) {
+                            case "功出电压V":
+                                measuredValue = quDuanInfoEntity.getVOutZhu();
+                                measuredValueUnit = "V";
+                                break;
+                            case "功出电流mA":
+                                measuredValue = quDuanInfoEntity.getMaOutZhu();
+                                measuredValueUnit = "mA";
+                                break;
+                            case "上边频Hz":
+                                measuredValue = quDuanInfoEntity.getHzUpZhu();
+                                measuredValueUnit = "Hz";
+                                break;
+                            case "下边频Hz":
+                                measuredValue = quDuanInfoEntity.getHzDownZhu();
+                                measuredValueUnit = "Hz";
+                                break;
+                            case "发送低频Hz":
+                                measuredValue = quDuanInfoEntity.getHzLowZhu();
+                                measuredValueUnit = "Hz";
+                                break;
+                            case "送端电缆侧电压V":
+                                measuredValue = quDuanInfoEntity.getVSongduanCable();
+                                measuredValueUnit = "V";
+                                break;
+                            case "送端电缆侧电流mA":
+                                measuredValue = quDuanInfoEntity.getMaSongduanCable();
+                                measuredValueUnit = "mA";
+                                break;
+                            case "受端电缆侧主电压V":
+                                measuredValue = quDuanInfoEntity.getVShouduanCableHost();
+                                measuredValueUnit = "V";
+                                break;
+                            case "受端电缆侧调电压V":
+                                measuredValue = quDuanInfoEntity.getVShouduanCableSpare();
+                                measuredValueUnit = "V";
+                                break;
+                            case "轨入电压V":
+                                measuredValue = quDuanInfoEntity.getVInAll();
+                                measuredValueUnit = "V";
+                                break;
+                            case "主接入电压mV":
+                                measuredValue = quDuanInfoEntity.getMvInZhu();
+                                measuredValueUnit = "mV";
+                                break;
 
-                        case "调接入电压mV":
-                            measuredValue = quDuanInfoEntity.getMvInDiaoZhu();
-                            measuredValueUnit = "mV";
-                            break;
-                        case "接收低频Hz":
-                            measuredValue = quDuanInfoEntity.getHzInLowZhu();
-                            measuredValueUnit = "Hz";
-                            break;
-                        case "FBP电缆电流mA":
-                            measuredValue = quDuanInfoEntity.getMaCableFbp();
-                            measuredValueUnit = "mA";
-                            break;
-                        case "FBP长内电流A":
-                            measuredValue = quDuanInfoEntity.getALonginFbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "FBP长外电流A":
-                            measuredValue = quDuanInfoEntity.getALongoutFbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "FBP短内电流A":
-                            measuredValue = quDuanInfoEntity.getAShortinFbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "FBP短外电流A":
-                            measuredValue = quDuanInfoEntity.getAShortoutFbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "FBP温度°C":
-                            measuredValue = quDuanInfoEntity.getTFbp();
-                            measuredValueUnit = "°C";
-                            break;
-                        case "FBA长内电流A":
-                            measuredValue = quDuanInfoEntity.getALonginFbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "FBA长外电流A":
-                            measuredValue = quDuanInfoEntity.getALongoutFbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "FBA短内电流A":
-                            measuredValue = quDuanInfoEntity.getAShortinFbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "FBA短外电流A":
-                            measuredValue = quDuanInfoEntity.getAShortoutFbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBA长内电流A":
-                            measuredValue = quDuanInfoEntity.getALonginJbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBA长外电流A":
-                            measuredValue = quDuanInfoEntity.getALongoutJbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBA短内电流A":
-                            measuredValue = quDuanInfoEntity.getAShortinJbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBA短外电流A":
-                            measuredValue = quDuanInfoEntity.getAShortoutJbaZhu();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBP电缆电流mA":
-                            measuredValue = quDuanInfoEntity.getMaCableJbp();
-                            measuredValueUnit = "mA";
-                            break;
-                        case "JBP长内电流A":
-                            measuredValue = quDuanInfoEntity.getALonginJbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBP长外电流A":
-                            measuredValue = quDuanInfoEntity.getALongoutJbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBP短内电流A":
-                            measuredValue = quDuanInfoEntity.getAShortinJbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBP短外电流A":
-                            measuredValue = quDuanInfoEntity.getAShortoutJbp();
-                            measuredValueUnit = "A";
-                            break;
-                        case "JBP温度°C":
-                            measuredValue = quDuanInfoEntity.getTJbp();
-                            measuredValueUnit = "°C";
-                            break;
-                        default:
-                            measuredValue = 0;
-                            measuredValueUnit = "";
+                            case "调接入电压mV":
+                                measuredValue = quDuanInfoEntity.getMvInDiaoZhu();
+                                measuredValueUnit = "mV";
+                                break;
+                            case "接收低频Hz":
+                                measuredValue = quDuanInfoEntity.getHzInLowZhu();
+                                measuredValueUnit = "Hz";
+                                break;
+                            case "FBP电缆电流mA":
+                                measuredValue = quDuanInfoEntity.getMaCableFbp();
+                                measuredValueUnit = "mA";
+                                break;
+                            case "FBP长内电流A":
+                                measuredValue = quDuanInfoEntity.getALonginFbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "FBP长外电流A":
+                                measuredValue = quDuanInfoEntity.getALongoutFbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "FBP短内电流A":
+                                measuredValue = quDuanInfoEntity.getAShortinFbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "FBP短外电流A":
+                                measuredValue = quDuanInfoEntity.getAShortoutFbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "FBP温度°C":
+                                measuredValue = quDuanInfoEntity.getTFbp();
+                                measuredValueUnit = "°C";
+                                break;
+                            case "FBA长内电流A":
+                                measuredValue = quDuanInfoEntity.getALonginFbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "FBA长外电流A":
+                                measuredValue = quDuanInfoEntity.getALongoutFbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "FBA短内电流A":
+                                measuredValue = quDuanInfoEntity.getAShortinFbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "FBA短外电流A":
+                                measuredValue = quDuanInfoEntity.getAShortoutFbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBA长内电流A":
+                                measuredValue = quDuanInfoEntity.getALonginJbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBA长外电流A":
+                                measuredValue = quDuanInfoEntity.getALongoutJbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBA短内电流A":
+                                measuredValue = quDuanInfoEntity.getAShortinJbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBA短外电流A":
+                                measuredValue = quDuanInfoEntity.getAShortoutJbaZhu();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBP电缆电流mA":
+                                measuredValue = quDuanInfoEntity.getMaCableJbp();
+                                measuredValueUnit = "mA";
+                                break;
+                            case "JBP长内电流A":
+                                measuredValue = quDuanInfoEntity.getALonginJbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBP长外电流A":
+                                measuredValue = quDuanInfoEntity.getALongoutJbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBP短内电流A":
+                                measuredValue = quDuanInfoEntity.getAShortinJbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBP短外电流A":
+                                measuredValue = quDuanInfoEntity.getAShortoutJbp();
+                                measuredValueUnit = "A";
+                                break;
+                            case "JBP温度°C":
+                                measuredValue = quDuanInfoEntity.getTJbp();
+                                measuredValueUnit = "°C";
+                                break;
+                            default:
+                                measuredValue = 0;
+                                measuredValueUnit = "";
+                        }
+                        menXianEntity.setMeasuredValue(measuredValue);
+                        menXianEntity.setMeasuredValueUnit(measuredValueUnit);
+                        menXianDao.insertSelective(menXianEntity);
                     }
-                    menXianEntity.setMeasuredValue(measuredValue);
-                    menXianEntity.setMeasuredValueUnit(measuredValueUnit);
-                    menXianDao.insertSelective(menXianEntity);
                 }
             }
         }
