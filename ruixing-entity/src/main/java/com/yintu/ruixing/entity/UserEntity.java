77e22afb8f36d1186d2c023e3781f2f8cc9cf19f
global_mwy <global_1107201045@qq.com>
2020-06-29 20:52:07 +0800

    Edit too much files

--- ruixing-entity/src/main/java/com/yintu/ruixing/entity/UserEntity.java
+++ ruixing-entity/src/main/java/com/yintu/ruixing/entity/UserEntity.java
@@ -49,7 +49,7 @@ public class UserEntity implements UserDetails {
 
     private Long departmentId;
 
-    private DepartmentEntity departmentEntities;
+    private DepartmentEntity departmentEntity;
 
 
     @Override
