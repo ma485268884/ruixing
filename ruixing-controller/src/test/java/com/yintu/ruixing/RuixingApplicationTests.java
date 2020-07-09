9e1ead2de315e2fd456231b4ce74f6d26469c1d5
global_mwy <global_1107201045@qq.com>
2020-07-09 18:21:31 +0800

    Edit too much files

--- ruixing-controller/src/test/java/com/yintu/ruixing/RuixingApplicationTests.java
+++ ruixing-controller/src/test/java/com/yintu/ruixing/RuixingApplicationTests.java
@@ -1,24 +1,34 @@
 package com.yintu.ruixing;
 
+import com.yintu.ruixing.entity.PreSaleFileAuditorEntity;
+import com.yintu.ruixing.service.PreSaleFileAuditorService;
 import com.yintu.ruixing.service.UserService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class RuixingApplicationTests {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private PreSaleFileAuditorService preSaleFileAuditorService;
 
 
-
-
-   /* @Test
+    @Test
     void contextLoads() {
-        Integer[] ids = new Integer[]{1, 2, 3};
-        List<SolutionStatusEntity> solutionStatusEntities = solutionStatusService.findByIdsAndType(ids, (short) 1);
-        System.out.println(solutionStatusEntities);
+        List<PreSaleFileAuditorEntity> preSaleFileAuditorEntities = new ArrayList<>();
+        PreSaleFileAuditorEntity preSaleFileAuditorEntity = new PreSaleFileAuditorEntity();
+        preSaleFileAuditorEntity.setPreSaleFileId(0);
+        preSaleFileAuditorEntity.setAuditorId(0);
+        preSaleFileAuditorEntity.setIsPass((short) 0);
+        preSaleFileAuditorEntities.add(preSaleFileAuditorEntity);
+        preSaleFileAuditorService.addMuch(preSaleFileAuditorEntities);
+    }
 
 
-    }*/
 }
