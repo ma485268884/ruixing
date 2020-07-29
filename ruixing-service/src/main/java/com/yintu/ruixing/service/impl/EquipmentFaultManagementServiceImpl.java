f2a986b93ac6806adddabcf0c4003f6c3029bb97
global_mwy <global_1107201045@qq.com>
2020-07-29 20:48:34 +0800

    Edit too much files

---
+++ ruixing-service/src/main/java/com/yintu/ruixing/service/impl/EquipmentFaultManagementServiceImpl.java
@@ -0,0 +1,57 @@
+package com.yintu.ruixing.service.impl;
+
+import com.yintu.ruixing.dao.EquipmentFaultManagementDao;
+import com.yintu.ruixing.entity.EquipmentFaultManagementEntity;
+import com.yintu.ruixing.service.EquipmentFaultManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author:mlf
+ * @date:2020/7/29 16:27
+ */
+@Service
+@Transactional
+public class EquipmentFaultManagementServiceImpl implements EquipmentFaultManagementService {
+    @Autowired
+    private EquipmentFaultManagementDao equipmentFaultManagementDao;
+
+    @Override
+    public void add(EquipmentFaultManagementEntity entity) {
+        equipmentFaultManagementDao.insertSelective(entity);
+    }
+
+    @Override
+    public void remove(Integer id) {
+        equipmentFaultManagementDao.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void edit(EquipmentFaultManagementEntity entity) {
+        equipmentFaultManagementDao.updateByPrimaryKeySelective(entity);
+    }
+
+    @Override
+    public EquipmentFaultManagementEntity findById(Integer id) {
+        List<EquipmentFaultManagementEntity> equipmentFaultManagementEntities = equipmentFaultManagementDao.connectSelectByCondition(id, null, null);
+        return equipmentFaultManagementEntities.isEmpty() ? null : equipmentFaultManagementEntities.get(0);
+    }
+
+    @Override
+    public void remove(Integer[] ids) {
+        for (Integer id : ids) {
+            this.remove(id);
+        }
+    }
+
+    @Override
+    public List<EquipmentFaultManagementEntity> findByStartDateAndEndDate(Date startDate, Date endDate) {
+        return equipmentFaultManagementDao.connectSelectByCondition(null, startDate, endDate);
+    }
+
+
+}
