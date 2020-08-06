ba97bdc7a1856453c8f35dbd250fb0b82672a83f
hxw-ly <1812538763@qq.com>
2020-08-07 00:00:12 +0800

    update entity

--- ruixing-entity/src/main/java/com/yintu/ruixing/entity/PermissionRoleEntity.java
+++ ruixing-entity/src/main/java/com/yintu/ruixing/entity/PermissionRoleEntity.java
@@ -4,37 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PermissionRoleEntity {
+public class PermissionRoleEntity implements Serializable {
+    private static final long serialVersionUID = 7667009621254933740L;
+
     private Long id;
 
     private Long roleId;
 
     private Long permissionId;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long roleId) {
-        this.roleId = roleId;
-    }
-
-    public Long getPermissionId() {
-        return permissionId;
-    }
-
-    public void setPermissionId(Long permissionId) {
-        this.permissionId = permissionId;
-    }
 }
--- ruixing-entity/src/main/java/com/yintu/ruixing/entity/UserRoleEntity.java
+++ ruixing-entity/src/main/java/com/yintu/ruixing/entity/UserRoleEntity.java
@@ -1,7 +1,13 @@
 package com.yintu.ruixing.entity;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRoleEntity implements Serializable {
     private static final long serialVersionUID = 5648734088166412767L;
     private Long id;
@@ -9,28 +15,4 @@ public class UserRoleEntity implements Serializable {
     private Long userId;
 
     private Long roleId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long roleId) {
-        this.roleId = roleId;
-    }
 }
