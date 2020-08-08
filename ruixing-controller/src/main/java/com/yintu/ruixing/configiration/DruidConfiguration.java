a084774140271f36aabf274047efa04a4071bd60
global_mwy <global_1107201045@qq.com>
2020-08-08 13:37:51 +0800

    Edit  too much files

--- ruixing-controller/src/main/java/com/yintu/ruixing/configiration/DruidConfiguration.java
+++ ruixing-controller/src/main/java/com/yintu/ruixing/configiration/DruidConfiguration.java
@@ -1,6 +1,7 @@
 package com.yintu.ruixing.configiration;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.xa.DruidXADataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
